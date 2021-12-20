05c68567ef4c4be867c388b5d73c2fdfdb4e616c
jurvo <jurvoel@gmx.de>
2021-12-20 09:12:24 +0100

    fixing small error in integration

--- py/simple test.py
+++ py/simple test.py
@@ -26,7 +26,7 @@ t_1_0, t_2_0 = np.pi/4., np.pi/2.
 v_1_0, v_2_0 = 0, 0
 
 delta_t = 1/30
-t_max = 5
+t_max = 5.9
 
 ### >>> END SETUP <<<
 
@@ -38,7 +38,8 @@ t_2[0] = t_2_0
 v_1[0] = v_1_0
 v_2[0] = v_2_0
 
-delta_v_1, delta_v_2 = 1, 1
+delta_v_1, delta_v_2 = 0, 0
+delta_v_1_temp, delta_v_2_temp = 0, 0
 
 """
 delta_v_1 = - m_2/(m_1+m_2) * l_2/l_1 * (delta_v_2 * np.cos(t_1_0 - t_2_0) + v_2_0**2*np.sin(t_1_0 - t_2_0)) - g/l_1 * np.sin(t_1_0)
@@ -53,8 +54,10 @@ c_4 = - g/l_2
 
 for i in range(1, len(t)):
     dt = t_1[i-1] - t_2[i-1]
-    delta_v_1 = c_1 * (delta_v_2 * np.cos(dt) + v_2[i-1]**2*np.sin(dt)) + c_2 * np.sin(t_1[i-1])
-    delta_v_2 = c_3 * (delta_v_1 * np.cos(dt) + v_1[i-1]**2*np.sin(dt)) + c_4 * np.sin(t_2[i-1])
+    delta_v_1_temp = c_1 * (delta_v_2 * np.cos(dt) + v_2[i-1]**2*np.sin(dt)) + c_2 * np.sin(t_1[i-1])
+    delta_v_2_temp = c_3 * (delta_v_1 * np.cos(dt) + v_1[i-1]**2*np.sin(dt)) + c_4 * np.sin(t_2[i-1])
+    delta_v_1 = delta_v_1_temp
+    delta_v_2 = delta_v_2_temp
     v_1[i] = v_1[i-1] + delta_t * delta_v_1
     t_1[i] = t_1[i-1] + delta_t * v_1[i]
     v_2[i] = v_2[i-1] + delta_t * delta_v_2
@@ -64,7 +67,7 @@ for i in range(1, len(t)):
 #print(t_2)
 
 
-"""
+
 fig = plt.figure()
 ax = plt.axes(xlim=(-(l_1+l_2), l_1+l_2), ylim=(-(l_1+l_2), l_1+l_2))
 line, = ax.plot([], [], lw=3)
@@ -88,7 +91,7 @@ anim = FuncAnimation(fig, animate, init_func=init,
 #anim.save('sine_wave.gif', writer='imagemagick')
 #anim.save('Pendulum_swing.mp4', fps=int(1/delta_t))
 plt.show()
-"""
+
 
 kin = np.zeros(len(t))
 pot = np.zeros(len(t))
