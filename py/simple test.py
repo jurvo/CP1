cdcdf90acbeb1197985f0a52b6f135137aa3d212
jurvo <jurvoel@gmx.de>
2021-12-27 20:30:32 +0100

    added verbose mode

--- py/simple test.py
+++ py/simple test.py
@@ -7,45 +7,46 @@ plt.style.use('seaborn-pastel')
 # TODO: Compare with analytical solution for simple pendulum
 ### >>> BEGIN SETUP <<<
 # mass
-m_1, m_2 = 1., 1.
+m_1, m_2 = 1, 1.
 # length of the pendulums
 l_1, l_2 = 1., 1.
 # gravity const
 g = 9.81
 
 # inital angles
-#t_1_0, t_2_0 = np.pi/2,np.pi/2
-t_1_0, t_2_0 = 2*np.pi/3., -np.pi/18.
+t_1_0, t_2_0 = np.pi/2,np.pi/2
+#t_1_0, t_2_0 = 2*np.pi/3., -np.pi/18.
 # inital momentum
 v_1_0, v_2_0 = 0.0, 0.0
 
 # in seconds
 delta_t = 0.001
-t_max = 300
+t_max = 100
 
-animate_pendulum = False
+animate_pendulum = True
 plot_energies = True
-verbose_mode = False
+verbose = True
 
 # 0 = Forward Euler, 1 = RK4
 simulation_mode = 1
 ### >>> END SETUP <<<
 
-print("Double Pendulum simulation:")
-print("m1:", m_1, "| m2:", m_2)
-print("l1:", l_1, "| l2:", l_2)
-print("g:", g)
-print("Initial Values:")
-print("t1:", t_1_0, "| t2:", t_2_0)
-print("v1:", v_1_0, "| v2:", v_2_0)
-print("Time settings: from 0 to", t_max, "seconds in ", delta_t, "sec steps")
-print("Simulation is done with:", "Forward Euler" if simulation_mode == 0 else "RK4")
+if verbose:
+    print("Double Pendulum simulation:")
+    print("m1:", m_1, "| m2:", m_2)
+    print("l1:", l_1, "| l2:", l_2)
+    print("g:", g)
+    print("Initial Values:")
+    print("t1:", t_1_0, "| t2:", t_2_0)
+    print("v1:", v_1_0, "| v2:", v_2_0)
+    print("Time settings: from 0 to", t_max, "seconds in ", delta_t, "sec steps")
+    print("Simulation is done with:", "Forward Euler" if simulation_mode == 0 else "RK4")
 
 # delcare and initilize arrays
 t = np.arange(0, t_max, delta_t)
 t_1, t_2, v_1, v_2, a_1, a_2 = np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)) # create data arrays
 
-# assinge initial values
+# assing initial values
 t_1[0], t_2[0] = t_1_0, t_2_0
 v_1[0], v_2[0] = v_1_0, v_2_0
 a_1[0], a_2[0] = 0., 0.
@@ -57,15 +58,14 @@ c_3 = - l_1/l_2
 c_4 = - g/l_2
 
 # a RK4 method
-def phi(i, u, h ,f):
-    k_1, k_2, k_3, k_4 = 0, 0, 0, 0
-    k_1 = f(i, u)
-    k_2 = f(i + h/2., u * h/2. * k_1)
-    k_3 = f(i + h/2., u * h/2. * k_2)
-    k_4 = f(i + h, u * h * k_3)
-    return (k_1 + 2. * k_2 + 2. * k_3 + k_4) / 6.
+def phi(u, h ,f):
+    k_1 = f(u)
+    k_2 = f(.5 * u * h * k_1)
+    k_3 = f(.5 * u * h * k_2)
+    k_4 = f(u * h * k_3)
+    return (k_1 + 2. * (k_2 + k_3) + k_4) / 6.
 
-def G(i, u):
+def G(u):
     t1 = u[0]
     t2 = u[1]
     v1 = u[2]
@@ -77,13 +77,10 @@ def G(i, u):
     delta = - c_3 * v1**2*np.sin(dt) + c_4*np.sin(t2)
     a1 = (alpha*delta + beta) / (1 - alpha * gamma)
     a2 = a1 * gamma + delta
-    #a_1[int(i)] = c_1 * (a_2[int(i-1)] * np.cos(dt) + v2*v2*np.sin(dt)) + c_2 * np.sin(t1)
-    #a_2[int(i)] = c_3 * (a_1[int(i-1)] * np.cos(dt) - v1*v1*np.sin(dt)) + c_4 * np.sin(t2)
-    #return np.array([v1,v2,a_1[int(i)],a_2[int(i)]])
     return np.array([v1, v2, a1, a2])
 
 # do the integration
-print("Start time integration")
+if verbose: print("Start time integration")
 if simulation_mode == 0: # Forward Euler
     for i in range(1, len(t)):
         dtheta = t_1[i-1] - t_2[i-1]
@@ -95,11 +92,14 @@ if simulation_mode == 0: # Forward Euler
         t_2[i] = t_2[i-1] + delta_t * v_2[i]
 elif simulation_mode == 1: # RK4
     for i in range(1, len(t)):
-        u = np.array([t_1[i-1], t_2[i-1], v_1[i-1], v_2[i-1]])
-        u += delta_t * phi(int(i), u, delta_t, G)
-        t_1[i], t_2[i], v_1[i], v_2[i] = u[0], u[1], u[2], u[3]
-print("Time integration done.")
-print("Calculating...")
+        p = delta_t * phi(np.array([t_1[i-1], t_2[i-1], v_1[i-1], v_2[i-1]]), delta_t, G)
+        t_1[i] = t_1[i-1] + p[0]
+        t_2[i] = t_2[i-1] + p[1]
+        v_1[i] = v_1[i-1] + p[2]
+        v_2[i] = v_2[i-1] + p[3]
+if verbose:
+    print("Time integration done.")
+    print("Calculating...")
 # calculating the x, y, e_kin, e_pot, e_tot values
 x_1 = l_1*np.sin(t_1)
 x_2 = x_1 + l_2*np.sin(t_2)
@@ -109,11 +109,11 @@ y_2 = y_1 - l_2*np.cos(t_2)
 E_kin = .5*m_1*l_1**2*v_1**2 + .5*m_2*(l_1**2*v_1**2 + l_2**2*v_2**2 + 2*v_1*v_2*l_1*l_2*np.cos(t_1 - t_2))
 E_pot = -g*(m_1*l_1*np.cos(t_1) + m_2*(l_1*np.cos(t_1) + l_2*np.cos(t_2)))
 E_tot = E_kin + E_pot
-print("Calculations done.")
-print("Start plotting.")
+if verbose:
+    print("Calculations done.")
+    print("Start plotting.")
 # plot the pendulum
 if animate_pendulum:
-
     fig = plt.figure(figsize=(8,8))
     ax = plt.axes(xlim=(-(l_1+l_2), l_1+l_2), ylim=(-(l_1+l_2), l_1+l_2))
     line, = ax.plot([], [], 'o-',lw=2)
@@ -147,4 +147,4 @@ if plot_energies:
     plt.ylim([-50,200])
     plt.legend()
     plt.show()
-print("Plotting done.")
+if verbose: print("Plotting done.")
