8ef8c3181eec7364d94608f2b1bdb7aa345d5c67
L-C-F-A <lennart_ahrens@unitybox.de>
2022-01-08 18:25:25 +0100

    Fractal plotting works
    Now, fractal plotting works, but calculation of "impossible values" takes long computing time, therefore a kind of Filter will be implemented, as known from Wikipedia.

--- py/simple test.py
+++ py/simple test.py
@@ -14,20 +14,21 @@ l_1, l_2 = 1., 1.
 g = 9.81
 
 # inital angles
-t_1_0, t_2_0 = np.pi/2 , np.pi/2
+t_1_0, t_2_0 =  np.pi/4 , - np.pi/4
 #t_1_0, t_2_0 = 2*np.pi/3., -np.pi/18.
 
 # initial value array values
 initmin = -( np.pi - 0.01) # -0.01 to avoid stable state in upright position 
 initmax = (np.pi -0.01)
 initres = 10 # resolution of measured points, for square array only
+#border_of_exacticity = 0.1
 
 # inital momentum
 v_1_0, v_2_0 = 0.0, 0.0
 
 # in seconds
 delta_t = 0.001
-t_max = 50
+t_max = 10
 
 animate_pendulum = True
 plot_energies = True
@@ -35,15 +36,15 @@ verbose = True
 plot_phasespace = True
 plot_phasespacekorr = False
 flipcounter = True
-calculate_fractal = False
+calculate_fractal = True #RK4 only, switches to RK4 automatically.
 
 # 0 = Forward Euler, 1 = RK4
 simulation_mode = 1
 ### >>> END SETUP <<<
 
 # initial value arrays
-t_1_init = np.linspace(initmin, initmax, initres)
-t_2_init = np.linspace(initmin, initmax, initres) # here obviously t_1_init could be used, but, if other shapes of arrays should be needed it's easier to build the code directly for two dims instead of a double use of one dim.
+t_1_init = np.linspace(initmin, initmax, initres, endpoint=True)
+t_2_init = np.linspace(initmin, initmax, initres, endpoint=True) # here obviously t_1_init could be used, but, if other shapes of arrays should be needed it's easier to build the code directly for two dims instead of a double use of one dim.
 
 if verbose:
     print("Double Pendulum simulation:")
@@ -59,72 +60,94 @@ if verbose:
 # delcare and initilize arrays
 t = np.arange(0, t_max, delta_t)
 t_1, t_2, v_1, v_2, a_1, a_2 = np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)), np.zeros(len(t)) # create data arrays
-fractal = []
+
 if calculate_fractal:
+    simulation_mode = 1
+    fractal = []
     for l in range(1, len(t_2_init)):
         fractal_row = []
+        breakvalue = t_max
+        breakvalue_rescure = breakvalue
         for m in range(1, len(t_1_init)):
             # assing initial values
             t_1[0], t_2[0] = t_1_init[m], t_2_init[l]
             v_1[0], v_2[0] = v_1_0, v_2_0
             a_1[0], a_2[0] = 0., 0.
-
-            # calculating the constants
-            c_1 = - m_2/(m_1+m_2) * l_2/l_1
-            c_2 = - g/l_1
-            c_3 = - l_1/l_2
-            c_4 = - g/l_2
-
-            # Additional values
-            flips_1 = 0.
-            flips_2 = 0.
-
-            # a RK4 method
-            def phi(u, h ,f):
-                k_1 = f(u)
-                k_2 = f(u + .5 * h * k_1)
-                k_3 = f(u + .5 * h * k_2)
-                k_4 = f(u + h * k_3)
-                return (k_1 + 2. * (k_2 + k_3) + k_4) / 6.
-
-            def G(u):
-                t1 = u[0]
-                t2 = u[1]
-                v1 = u[2]
-                v2 = u[3]
-                dt = t1-t2
-                alpha = c_1 * np.cos(dt)
-                beta = c_1 * v2**2*np.sin(dt) + c_2*np.sin(t1)
-                gamma = c_3 * np.cos(dt)
-                delta = - c_3 * v1**2*np.sin(dt) + c_4*np.sin(t2)
-                a1 = (alpha*delta + beta) / (1 - alpha * gamma)
-                a2 = a1 * gamma + delta
-                return np.array([v1, v2, a1, a2])
-
-            # do the integration
-            if verbose: print("Start time integration for t_1_init =", t_1[0], " and t_2_init =", t_2[0])
-            if simulation_mode == 0: # Forward Euler
-                for i in range(1, len(t)):
-                    dtheta = t_1[i-1] - t_2[i-1]
-                    a_1[i] = c_1 * (a_2[i-1] * np.cos(dtheta) + v_2[i-1]*v_2[i-1]*np.sin(dtheta)) + c_2 * np.sin(t_1[i-1])
-                    a_2[i] = c_3 * (a_1[i-1] * np.cos(dtheta) - v_1[i-1]*v_1[i-1]*np.sin(dtheta)) + c_4 * np.sin(t_2[i-1])
-                    v_1[i] = v_1[i-1] + delta_t * a_1[i]
-                    v_2[i] = v_2[i-1] + delta_t * a_2[i]
-                    t_1[i] = t_1[i-1] + delta_t * v_1[i]
-                    t_2[i] = t_2[i-1] + delta_t * v_2[i]
-            elif simulation_mode == 1: # RK4
-                for i in range(1, len(t)):
-                    p = delta_t * phi(np.array([t_1[i-1], t_2[i-1], v_1[i-1], v_2[i-1]]), delta_t, G)
-                    t_1[i] = t_1[i-1] + p[0]
-                    t_2[i] = t_2[i-1] + p[1]
-                    v_1[i] = v_1[i-1] + p[2]
-                    v_2[i] = v_2[i-1] + p[3]
-            fractal_row.append()
             
-    if verbose:
-        print("Time integration done.")
-        print("Calculating...")
+            #if (3*np.cos(t_1[0])+np.cos(t_2[0])) > border_of_exacticity:
+            #    fractal_row.append(breakvalue_rescure)
+            #else:
+            if True:           
+                # calculating the constants
+                c_1 = - m_2/(m_1+m_2) * l_2/l_1
+                c_2 = - g/l_1
+                c_3 = - l_1/l_2
+                c_4 = - g/l_2
+
+                # Additional values
+                flips_1 = 0.
+                flips_2 = 0.
+
+                # a RK4 method
+                def phi(u, h ,f):
+                    k_1 = f(u)
+                    k_2 = f(u + .5 * h * k_1)
+                    k_3 = f(u + .5 * h * k_2)
+                    k_4 = f(u + h * k_3)
+                    return (k_1 + 2. * (k_2 + k_3) + k_4) / 6.
+
+                def G(u):
+                    t1 = u[0]
+                    t2 = u[1]
+                    v1 = u[2]
+                    v2 = u[3]
+                    dt = t1-t2
+                    alpha = c_1 * np.cos(dt)
+                    beta = c_1 * v2**2*np.sin(dt) + c_2*np.sin(t1)
+                    gamma = c_3 * np.cos(dt)
+                    delta = - c_3 * v1**2*np.sin(dt) + c_4*np.sin(t2)
+                    a1 = (alpha*delta + beta) / (1 - alpha * gamma)
+                    a2 = a1 * gamma + delta
+                    return np.array([v1, v2, a1, a2])
+
+                # do the integration
+                if verbose: print("Start time integration for t_1_init =", m, " and t_2_init =", l)
+                if simulation_mode == 0: # Forward Euler
+                    for i in range(1, len(t)):
+                        dtheta = t_1[i-1] - t_2[i-1]
+                        a_1[i] = c_1 * (a_2[i-1] * np.cos(dtheta) + v_2[i-1]*v_2[i-1]*np.sin(dtheta)) + c_2 * np.sin(t_1[i-1])
+                        a_2[i] = c_3 * (a_1[i-1] * np.cos(dtheta) - v_1[i-1]*v_1[i-1]*np.sin(dtheta)) + c_4 * np.sin(t_2[i-1])
+                        v_1[i] = v_1[i-1] + delta_t * a_1[i]
+                        v_2[i] = v_2[i-1] + delta_t * a_2[i]
+                        t_1[i] = t_1[i-1] + delta_t * v_1[i]
+                        t_2[i] = t_2[i-1] + delta_t * v_2[i]
+                elif simulation_mode == 1: # RK4
+                    for i in range(1, len(t)):
+                        breakvalue = i * delta_t
+                        p = delta_t * phi(np.array([t_1[i-1], t_2[i-1], v_1[i-1], v_2[i-1]]), delta_t, G)
+                        t_1[i] = t_1[i-1] + p[0]
+                        t_2[i] = t_2[i-1] + p[1]
+                        v_1[i] = v_1[i-1] + p[2]
+                        v_2[i] = v_2[i-1] + p[3]
+                        if t_1[i]>np.pi  or t_1[i]<-np.pi or t_2[i]>np.pi or t_2[i]<-np.pi :
+                            break
+                if verbose:
+                    print("m=", m, "=", t_1[0], "| l=", l, "=", t_1[0], "breakvalue =", breakvalue )        
+                fractal_row.append(breakvalue)
+        print(fractal_row)
+        fractal.append(fractal_row)
+    print(fractal)
+    
+    #Plot as colour-coded field of squares
+    
+    fig = plt.figure()
+    plt.pcolormesh(fractal)
+    plt.show()
+    
         
+            
+    
+#%%        
 else:
     # assing initial values
     t_1[0], t_2[0] = t_1_0, t_2_0
@@ -254,7 +277,7 @@ else:
         plt.plot(t_2, flips_2, label="Flips Pendulum 2")
         plt.legend()
         plt.show()
-
+#%%
 
     
 
