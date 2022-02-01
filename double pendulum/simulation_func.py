1352e89fe31d7dd214a19ed8dbd8c71d98f3a33d
jurvo <jurvoel@gmx.de>
2022-02-01 16:45:55 +0100

    update with np.array

--- double pendulum/simulation_func.py
+++ double pendulum/simulation_func.py
@@ -39,12 +39,14 @@ def simulate(m_1, m_2, l_1, l_2, g, t_1_0, t_2_0, v_1_0, v_2_0, t_max, delta_t, 
 	# delcare and initilize arrays
 	t = np.arange(0, t_max, delta_t)
 	n = len(t)
-	t_1, t_2, v_1, v_2, a_1, a_2 = np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n)
+	p = np.zeros((6, n))
 
 	# assing initial values
-	t_1[0], t_2[0] = t_1_0, t_2_0
-	v_1[0], v_2[0] = v_1_0, v_2_0
-	a_1[0], a_2[0] = 0., 0.
+	p[0, 0] = t_1_0
+	p[1, 0] = t_2_0
+	p[2, 0] = v_1_0
+	p[3, 0] = v_2_0
+	p[4, 0], p[5, 0] = 0., 0.
 
 	# calculating the constants
 	c_1 = - m_2/(m_1+m_2) * l_2/l_1
@@ -56,25 +58,22 @@ def simulate(m_1, m_2, l_1, l_2, g, t_1_0, t_2_0, v_1_0, v_2_0, t_max, delta_t, 
 	if verbose: print("Start time integration")
 	if simulation_mode == 0: # Forward Euler
 		for i in range(1, n):
-			dtheta = t_1[i-1] - t_2[i-1]
-			a_1[i] = c_1 * (a_2[i-1] * np.cos(dtheta) + v_2[i-1]*v_2[i-1]*np.sin(dtheta)) + c_2 * np.sin(t_1[i-1])
-			a_2[i] = c_3 * (a_1[i-1] * np.cos(dtheta) - v_1[i-1]*v_1[i-1]*np.sin(dtheta)) + c_4 * np.sin(t_2[i-1])
-			v_1[i] = v_1[i-1] + delta_t * a_1[i]
-			v_2[i] = v_2[i-1] + delta_t * a_2[i]
-			t_1[i] = t_1[i-1] + delta_t * v_1[i]
-			t_2[i] = t_2[i-1] + delta_t * v_2[i]
+			dt = p[0, i-1] - p[1, i-1]
+			p[4, i] = c_1 * (p[5, i-1] * np.cos(dt) + p[3, i-1]**2*np.sin(dt)) + c_2 * np.sin(p[0,i-1])
+			p[5, i] = c_1 * (p[4, i-1] * np.cos(dt) + p[2, i-1]**2*np.sin(dt)) + c_2 * np.sin(p[1,i-1])
+			p[2, i] = p[2, i-1] + delta_t * p[4, i]
+			p[3, i] = p[3, i-1] + delta_t * p[5, i]
+			p[0, i] = p[0, i-1] + delta_t * p[2, i]
+			p[1, i] = p[1, i-1] + delta_t * p[3, i]
 	elif simulation_mode == 1: # RK4
 		for i in range(1, n):
-			p = delta_t * phi(np.array([t_1[i-1], t_2[i-1], v_1[i-1], v_2[i-1]]), delta_t, G, c_1, c_2, c_3, c_4)
-			t_1[i] = t_1[i-1] + p[0]
-			t_2[i] = t_2[i-1] + p[1]
-			v_1[i] = v_1[i-1] + p[2]
-			v_2[i] = v_2[i-1] + p[3]
+			tempP = delta_t * phi(p[0:4, i-1] , delta_t, G, c_1, c_2, c_3, c_4)
+			p[0:4, i] = p[0:4, i-1] + tempP
 
 	if verbose:
 		print("Time integration done.")
 		print("Saving...")
-	saveDataToFile(filename, t, t_1, t_2, v_1, v_2, m_1, m_2, l_1, l_2, g, t_max, delta_t, simulation_mode)
+	saveDataToFile(filename, t, p[0], p[1], p[2], p[3], m_1, m_2, l_1, l_2, g, t_max, delta_t, simulation_mode)
 	if verbose: print("Saving done!")
 
 ### >>> BEGIN SETUP <<<
@@ -99,45 +98,7 @@ verbose = False
 
 # 0 = Forward Euler, 1 = RK4
 simulation_mode = 1
-#filename = "default1.txt"
+filename = "default.txt"
 ### >>> END SETUP <<<
 
-simulate(m_1, m_2, l_1, l_2, g, t_1_0, t_2_0, v_1_0, v_2_0, t_max, delta_t, simulation_mode, verbose, "default.txt")
-
-"""
-def thread_func(x, y):
-	print(x, y)
-	simulate(m_1, m_2, l_1, l_2, g, x, y, v_1_0, v_2_0, t_max, delta_t, simulation_mode, verbose, "fractal\\" + str(x) + ";" + str(y) +".txt")
-	print("done")
-	return
-
-def work(foo):
-    foo.do_task()
-
-from multiprocessing import Pool
-
-class t:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-	def do_task(self):
-		print(self.x, self.y)
-		simulate(m_1, m_2, l_1, l_2, g, self.x, self.y, v_1_0, v_2_0, t_max, delta_t, simulation_mode, verbose, "fractal\\" + str(self.x) + ";" + str(self.y) +".txt")
-		print("done")
-
-init = np.linspace(start = -np.pi, stop =  np.pi, num = 50, endpoint = False)
-
-tasks = []
-for x in init:
-	for y in init:
-		tasks.append(t(x,y))
-
-if __name__ == '__main__':
-
-	pool = Pool()
-	pool.map(work, tasks)
-	pool.close()
-	pool.join()
-
-"""
+simulate(m_1, m_2, l_1, l_2, g, t_1_0, t_2_0, v_1_0, v_2_0, t_max, delta_t, simulation_mode, verbose, filename)
